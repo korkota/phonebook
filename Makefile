7a6e0a224a0d8b9739702a6a3d53e84f444b0ee8
korkota <alexey.korkota@gmail.com>
2014-02-11 14:49:29 +0400

    Added tests.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 MOCHA = ./node_modules/.bin/mocha
 
 test:
-    @NODE_ENV=test $(MOCHA) \
-        -r should \
-        -R spec
+	@NODE_ENV=test $(MOCHA) \
+		-r should \
+		-R spec
 
 .PHONY: test
--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var log = require('./lib/log')(module);
 app.listen(config.get('application:port'), function() {
   log.info('Express server listening on port ' + config.get('application:port'));
 });
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
